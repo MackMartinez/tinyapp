4287c154ace9dd14a02ce2eca1492b6c58701016
MackyClap <martimac2320@gmail.com>
2022-09-20 20:54:04 -0400

    revert post to res.send ok

--- express_server.js
+++ express_server.js
@@ -47,7 +47,7 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send(generateRandomString()); // Respond with 'Ok' (we will replace this)
+  res.send("ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req,res) => {
