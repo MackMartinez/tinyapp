88074cf28d88b2abf026d122e3459140b98d37ac
MackyClap <martimac2320@gmail.com>
2022-09-23 16:44:56 -0400

    add form element to handle post and add delete route

--- express_server.js
+++ express_server.js
@@ -52,6 +52,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect(`/urls`);
+})
+
 app.get("/urls/:id", (req,res) => {
   const id = req.params.id;
   const templateVars = { id, longURL: urlDatabase };
